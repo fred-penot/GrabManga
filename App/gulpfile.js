26120634982e1ca9f9ea13e5f1dba6f7b5bbae37
fred-penot <frederic.penot@gmail.com>
2016-11-11 20:41:40 +0100

    initial v0.1a

--- App/gulpfile.js
+++ App/gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-//var gutil = require('gulp-util');
-//var bower = require('bower');
+var gutil = require('gulp-util');
+var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-//var sh = require('shelljs');
+var sh = require('shelljs');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -21,7 +21,6 @@ gulp.task('sass', function(done) {
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
@@ -30,7 +29,7 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
-/*gulp.task('install', ['git-check'], function() {
+gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
@@ -49,4 +48,3 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
-*/
